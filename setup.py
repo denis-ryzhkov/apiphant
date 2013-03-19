27d7ffeecdfd5069ca180de90e8baad481cbccec
Denis Ryzhkov <denisr@denisr.com>
2013-03-19 22:40:02 +0300

    Version 0.1.1.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='apiphant',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple Python Web API framework, based on Gevent, JSON, CRUD.',
     long_description='''
 Features:
